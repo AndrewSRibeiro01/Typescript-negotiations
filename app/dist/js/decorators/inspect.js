2c00e1f50b50dc28ad3dc3ebfd2ea2973c57f0dc
AndrewSRB <116013992+AndrewSRibeiro01@users.noreply.github.com>
2025-01-28 13:58:48 -0300

    fix: add new decorator escape (#1)
    Co-authored-by: Andrew Souza <andrew.souza@aigroup.local>

--- app/dist/js/decorators/inspect.js
+++ app/dist/js/decorators/inspect.js
@@ -1,13 +1,12 @@
-export const inspect = () => {
-    return function (target, propertyKey, descriptor) {
-        const metodoOriginal = descriptor.value;
-        descriptor.value = function (...args) {
-            console.log(`--- Método ${propertyKey}`);
-            console.log(`------ Parãmetros ${JSON.stringify(args)}`);
-            const retorno = metodoOriginal.apply(this, args);
-            console.log(`--------- retorno ${JSON.stringify(retorno)}`);
-            return retorno;
-        };
-        return descriptor;
+export function inspect(target, propertyKey, descriptor) {
+    const metodoOriginal = descriptor.value;
+    descriptor.value = function (...args) {
+        console.log(`--- Método ${propertyKey}`);
+        console.log(`------ Parãmetros ${JSON.stringify(args)}`);
+        const retorno = metodoOriginal.apply(this, args);
+        console.log(`--------- retorno ${JSON.stringify(retorno)}`);
+        return retorno;
     };
-};
+    return descriptor;
+}
+;
